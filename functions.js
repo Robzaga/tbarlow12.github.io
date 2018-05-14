8304d7c2576d689bf33189e7585c37601e41be53
Rob <polunza@yahoo.com>
2018-05-14 13:55:54 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
 
 function sendEmail(){
-	window.open('mailto:rob.unzaga@gmail.com?subject=Test&body=Hey Tanner, How you doing?')
+	window.open('mailto:rob.unzaga@gmail.com?subject=Test&body=Hey Rob, How are you doing?')
 }
